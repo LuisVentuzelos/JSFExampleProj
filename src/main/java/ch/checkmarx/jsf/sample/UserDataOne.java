efb9082bed63327605831c765b39e418b10cdd6e
Luís Fernando Pereira Ventuzelos <luis.ventuzelos@checkmarx.com>
2021-08-23 14:51:09 +0100

    third commit

--- src/main/java/ch/checkmarx/jsf/sample/UserDataOne.java
+++ src/main/java/ch/checkmarx/jsf/sample/UserDataOne.java
@@ -4,17 +4,17 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import java.io.Serializable;
 
-@ManagedBean(name = "UserDataMany", eager = true)
+@ManagedBean(name = "UserDataOne", eager = true)
 @SessionScoped
-public class UserDataMany implements Serializable {
+public class UserDataOne implements Serializable {
     private static final long serialVersionUID = 1L;
-    public String[] data = {"1","2","3"};
+    public String data = "1";
 
-    public String[] getData() {
+    public String getData() {
         return data;
     }
 
-    public void setData(String[] data) {
+    public void setData(String data) {
         this.data = data;
     }
 }
